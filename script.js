ff5fef77044f22eea7a610f422cc2025e350d3e4
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2022-10-13 20:44:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstName = 'Brian';
+
+let lastName = 'Leithead';
+
+let fullNAme = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 639111;
+
+console.log(population);
+
+console.log(3 * population);
+
+let javaSciptIsCool = true;
+
+let colors = ["Red", "Orange", "Yellow" , "Green" , "Blue", "Indigo" , "Violet"];
+
+
